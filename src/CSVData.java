90d6547b0b02839cdf2dbe20e12fa0615316b116
Shivani Oghanta <shivani.oghanta@gmail.com>
2016-10-12 10:41:06 -0700

    add more method stubs

--- src/CSVData.java
+++ src/CSVData.java
@@ -6,25 +6,29 @@
 public class CSVData {
 	private double[][] data;
 	private String[] columnNames;
-	
+
 	public CSVData() {
 
-		
 	}
-	
+
 	/***
-	 * returns a new CSVData object for a file ignoring the lines at the top
-	 * It uses the first row as the column names. all other data is stored in 2D array
-	 * @param filename name of the CSV file
-	 * @param numLinesIgnore the number of lines to ignore from the top of the file
-	 * @param columnHeaders the names/descriptions of each column of values
+	 * returns a new CSVData object for a file ignoring the lines at the top It
+	 * uses the first row as the column names. all other data is stored in 2D
+	 * array
+	 * 
+	 * @param filename
+	 *            name of the CSV file
+	 * @param numLinesIgnore
+	 *            the number of lines to ignore from the top of the file
+	 * @param columnHeaders
+	 *            the names/descriptions of each column of values
 	 * @return CSVData
 	 */
-	public static CSVData readCSVFile(String filename, int numLinesIgnore, String[] columnHeaders){
+	public static CSVData readCSVFile(String filename, int numLinesIgnore, String[] columnHeaders) {
 		return new CSVData();
 	}
-	
-	public static CSVData readCSVFile(String filename, int numLinesIgnore){
+
+	public static CSVData readCSVFile(String filename, int numLinesIgnore) {
 		return new CSVData();
 	}
 
