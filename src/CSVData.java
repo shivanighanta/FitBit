4c022a9cbd7214df58a3e0de910c86560cb4163f
Shivani Oghanta <shivani.oghanta@gmail.com>
2016-10-12 10:40:01 -0700

    add method stubs to CSVData

--- src/CSVData.java
+++ src/CSVData.java
@@ -4,10 +4,28 @@
  *
  */
 public class CSVData {
-
-	public static void main(String[] args) {
+	private double[][] data;
+	private String[] columnNames;
+	
+	public CSVData() {
 
 		
+	}
+	
+	/***
+	 * returns a new CSVData object for a file ignoring the lines at the top
+	 * It uses the first row as the column names. all other data is stored in 2D array
+	 * @param filename name of the CSV file
+	 * @param numLinesIgnore the number of lines to ignore from the top of the file
+	 * @param columnHeaders the names/descriptions of each column of values
+	 * @return CSVData
+	 */
+	public static CSVData readCSVFile(String filename, int numLinesIgnore, String[] columnHeaders){
+		return new CSVData();
+	}
+	
+	public static CSVData readCSVFile(String filename, int numLinesIgnore){
+		return new CSVData();
 	}
 
 }
